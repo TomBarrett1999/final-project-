c82d375f01d51dec689b52ee6c2070fdbf711f93
TomBarrett1999 <39537769+TomBarrett1999@users.noreply.github.com>
2018-05-22 22:52:08 +0100

    Add files via upload
    adding a class to my object orientated code

---
+++ MaIn.java
@@ -0,0 +1,13 @@
+public class MaIn
+{
+	public static void main(String[] args)
+	{
+		GameArena g = new GameArena(1000,1000);
+/*
+*class that calls coursework 
+*and runs all the code within the file
+*allowing for game to be seen 
+*within the window*/ 
+ 
+    
+    		
